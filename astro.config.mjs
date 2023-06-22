b507bf23a18f68bce73bea29a9e0e7fd12a46c37
Trevor <tdmichaelis@gmail.com>
2023-06-22 08:16:55 -0600

    add github pages yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tdmichaelis.github.io',
+  base: '/nicosfresh',
   integrations: [
     tailwind(),
     image({
